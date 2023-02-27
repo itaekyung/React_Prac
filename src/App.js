af0ea83a805ec57e64328404f4a3381dfddde101
LeeTaeKyung <leek9708@gmail.com>
2023-02-27 17:50:27 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let post = "강남 우동 맛집";
+  let [글제목, b] = useState(["html&css 부수기", "JS 부수기", "리액트 부수기"]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>태키의 블로그</div>
+      </div>
+      <div className="list">
+        <h4>{글제목[0]}</h4>
+        <p>2월 27일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 27일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 27일 발행</p>
+      </div>
     </div>
   );
 }
