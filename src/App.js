e34a44bb6be902385bbff0964e0ffefb594437b3
LeeTaeKyung <leek9708@gmail.com>
2023-02-28 11:11:28 +0900

    ui

--- src/App.js
+++ src/App.js
@@ -9,20 +9,20 @@ function App() {
     "React 부수기",
   ]);
   let [like, setLike] = useState(0);
-
+  let [modal, setModal] = useState(false); // UI 현재 상태를 저장 형식은  자유
   return (
     <div className="App">
-      <div className="black-nav">태키의 블로그</div>
+      <Header />{" "}
+      <button
+        onClick={() => {
+          const newTitle = [...title];
+          newTitle.sort();
+          setTitle(newTitle);
+        }}
+      >
+        abc순서로 정렬
+      </button>
       <div className="list">
-        <button
-          onClick={() => {
-            const newTitle = [...title];
-            newTitle.sort();
-            setTitle(newTitle);
-          }}
-        >
-          abc순서로 정렬
-        </button>
         <h4>
           {title[0]}{" "}
           <span
@@ -35,30 +35,6 @@ function App() {
           {like}
         </h4>
         <button
-          // onClick={() => {
-          //   title[0] = "부쉈다";
-          //   console.log(title);
-          //   setTitle(title);
-          // }}\
-
-          // onClick={() => {
-          //   const test = title;
-          //   test[0] = "부쉈다";
-          //   console.log(test);
-          //   console.log(test == title);
-          //   setTitle(test);
-          // }}
-
-          // 위에 코드가 작동이 안되는 이유는 참조값을 변경하지 않아서 그런건가요?
-          // test랑 title이 같은 참조값을 가지고 있어서?
-          // useState는 그럼 초기값하고 변경값 ==이든, ===이든 상관없이 true인가요?
-
-          // onClick={() => {
-          //   title[0] = "부쉈다";
-          //   console.log([...title]);
-          //   setTitle([...title]);
-          // }}
-
           onClick={() => {
             const test = [...title];
             test[0] = "부쉈다";
@@ -73,11 +49,38 @@ function App() {
         <h4>{title[1]}</h4>
         <p>2월 27일 발행</p>
       </div>
-
       <div className="list">
-        <h4>{title[2]}</h4>
+        <h4
+          onClick={() => {
+            // if나 삼항연산자 써서 이 안에 작성해도되지만
+            // if (modal == true) {
+            //   return setModal(false);
+            // } else {
+            //   return setModal(true);
+            // }
+
+            setModal(!modal); // 이게 더 간단함
+          }}
+        >
+          {title[2]}
+        </h4>
         <p>2월 27일 발행</p>
       </div>
+      {modal == true ? <Modal /> : null}
+    </div>
+  );
+}
+
+function Header() {
+  return <div className="black-nav">태키의 블로그</div>;
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
